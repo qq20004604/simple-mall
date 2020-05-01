e4c2785ffa001d26255e17ba990b63523bf74fc9
qq20004604 <20004604@qq.com>
2020-05-01 13:02:00 +0800

    update

--- Server/reset_password/views.py
+++ Server/reset_password/views.py
@@ -4,7 +4,7 @@ from package.request_method_limit import post_limit
 from package.response_data import get_res_json
 from .forms import ResetVerifyCodeForm
 from package.send_sms import send_vcode_sms
-from register.models import TelVerifyCode
+from .models import ResetVerifyCode
 from configuration.variable import SMS_SEND_INTERVAL_TIME
 
 
@@ -30,7 +30,7 @@ def send_verify_code(request):
     # 先手机号码
     tel = uf.data['tel']
     # 1、倒叙查找查找号码符合的最新的一条数据
-    filter_resuilt = TelVerifyCode.objects.order_by('-id').filter(tel=tel)[:1]
+    filter_resuilt = ResetVerifyCode.objects.order_by('-id').filter(tel=tel)[:1]
     # 查到了，则查看是否小于最大间隔
     if len(filter_resuilt) > 0:
         if filter_resuilt[0].over_interval() is False:
@@ -39,7 +39,7 @@ def send_verify_code(request):
 
 
     # 2、生成一条数据
-    insert_data = TelVerifyCode.objects.create(tel=tel)
+    insert_data = ResetVerifyCode.objects.create(tel=tel)
     # 拿到验证码
     vcode = insert_data.vcode
     # 插入到数据库
@@ -48,6 +48,7 @@ def send_verify_code(request):
     # 成功返回True，失败返回错误提示信息
     # send_result = send_vcode_sms(tel, vcode)
     send_result = True
+    insert_data.is_over_max_daily()
 
     if send_result is True:
         # 成功
